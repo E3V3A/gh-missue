1d43fef44a271d8d4260981fa3e286a016525863
E3V3A <xdae3v3a@gmail.com>
2018-04-10 00:21:22 +0300

    :gem:

---
+++ README.md
@@ -0,0 +1,171 @@
+### gh-missue
+
+[![DocStatus](https://inch-ci.org/github/E3V3A/gh-missue.svg?branch=master)](https://inch-ci.org/github/E3V3A/gh-missue)
+[![GitHub last commit](https://img.shields.io/github/last-commit/E3V3A/gh-missue.svg)](https://github.com/E3V3A/gh-missue)
+[![Maintenance](https://img.shields.io/badge/Maintained%3F-yes-green.svg)](https://GitHub.com/E3V3A/gh-missue/graphs/commit-activity)
+[![Average time to resolve an issue](http://isitmaintained.com/badge/resolution/E3V3A/gh-missue.svg)](http://isitmaintained.com//project/E3V3A/gh-missue "Average time to resolve an issue")
+[![Dependency Status](https://beta.gemnasium.com/badges/github.com/(h.svg)](https://beta.gemnasium.com/projects/github.com/E3V3A/gh-missue)
+
+A complete CLI GitHub issue migration tool written in Ruby.
+
+
+| STATUS: | Version | Date | Maintained? |
+|:------- |:------- |:---- |:----------- |
+| Working | `1.0.1` | 2018-04-10 | YES |
+
+---
+
+**Q:** *What does `gh-missue` do?**
+
+The primary use is for migrating selected issues from any one repository to another.
+But it can do much more. You can also:
+
+- [x] Migrate *issues*, their labels and their comments in correct order.
+- [x] List any/all issues in any puplic repository
+- [x] List or migrate only issues selected by their *status*: `[all, open, closed]`
+- [x] List all available issue *labels* for any repository
+- [x] Copy all available issue *labels* for any repository, including: `name, color, description`.
+- [x] Use 3 different types of GitHub authentication: (*none, OAuth2 token, username/password*)
+- [x] Test your current GitHub request status showing your: *rate limit, ramining requests, quota refresh time*.
+- [x] Test your authentication token
+- [x] Learn about Ruby CLI options
+- [ ] Decide to keep originally closed issues as closed or opened.
+- [ ] Use the included class in your own tools
+
+
+**Q:** *What it does **not** do?*
+
+- Does not close issues on source repository after move
+- Does not copy time-stamps. The new time is when the issue was moved.
+- Does not copy issue-author. You will be the new author of all moved issues.
+- Does not copy comment-authors. You will be the new author of all moved issue comments.
+- Does not copy PR's. (But script can be easily modified to do so.)
+
+**Q:** *Why is this needed?*
+
+Sometimes, the structure of your project changes so drastically that it would break your repository.
+You need an easy way to start from scratch and just commit everything to a new repository.
+But, you've got all these valuable issues in the old repository on Github. 
+
+
+**Q:** *Why are you using Ruby?*
+
+I have never used Ryby until a few evenings ago. I came across an old library to migrate issues on github. 
+However, it was half broken and extremely limited. But using a library sucked and I wanted a proper CLI 
+that could handle large request rates. I decided to hack into it. Ruby is a nice suprisingly robust 
+language and it is still alive. Not surprsingly it is used by GitHub themselves.
+
+**Q:** *Will I continue to support this tool?*
+
+Sure, why not, but I will not spend any more time for new features. So if you wanna add something 
+please send me a PR.
+
+---
+
+### Dependencies
+
+This tool depends on:
+
+- [1] [docopt](https://github.com/docopt/docopt.rb/)  -- For amazingly cool command line option handling
+- [2] [octokit](https://github.com/octokit/octokit.rb/) -- For GitHub API access
+- [3] [json]()  -- 
+- [4] [net/http]()  -- 
+- [5] [uri]() -- 
+
+
+### Installation 
+
+
+1. To make this run, you need to:
+   (a) have Ruby installed
+   (b) gem install GitHubs own "octokit" library
+   (c) gem install the option parser "docopt"
+2. You should also consider creating a personal authentication token on GitHub,  
+   to avoid getting rate-limited by a large number of requests in short time.
+
+
+
+#### Installing Ruby on a RPi3
+
+Installing Ruby on a Raspbian OS can be slightly tricky. There are essentailly 2 methods to do this.
+1. Installing the APK package called `Ryby3`..
+2. Installing Ruby from sources
+
+I strongly recommend to use the first option, unless you plan to use Ruby a lot in the future, and to save a lot of time.
+
+...
+TBA
+
+
+
+#### Installing gh-missue
+
+```bash
+git clone https://github.com/E3V3A/gh-missue.git
+cd gh-missue
+bundle install
+
+```
+
+
+#### Bugs and Warnings
+
+None
+
+:information_source: For other bugs, issues, details and updates, please refer to the
+[issue tracker](https://github.com/eouia/MMM-Assistant/issues).
+
+
+#### Contribution
+
+Feel free to post issues and PR's related to this tool.
+Feel free to fork, break, fix and contribute. Enjoy!
+
+
+### Recommended similar tools
+
+* [github-issues-import](https://github.com/muff1nman/github-issues-import) and [mod](https://github.com/ericnewton76/github-issues-import) (Python)
+* [github-issue-mover](https://github.com/google/github-issue-mover) (Dart)
+* [offline-issues](https://github.com/jlord/offline-issues) (JS) -- To read issues offline
+* []() ()
+* []() ()
+
+References:
+
+* [Ruby in 20 minutes](https://www.ruby-lang.org/en/documentation/quickstart/)
+* [Installing Ruby on Rail on RPi3](http://jeanbrito.com/2017/01/23/installing-ruby2-4-on-rails5-environment-on-raspberry-pi-3/)
+
+
+---
+
+Essential GitHub API documents:
+
+* [Labels-used-for-issues](https://github.com/dotnet/roslyn/wiki/Labels-used-for-issues)
+* https://developer.github.com/v3/issues/
+* https://developer.github.com/v3/issues/labels/
+* https://developer.github.com/v3/issues/labels/#get-a-single-label
+* https://developer.github.com/v3/issues/#list-issues-for-a-repository
+* https://developer.github.com/v3/guides/best-practices-for-integrators/#dealing-with-rate-limits
+
+* https://developer.github.com/v3/#abuse-rate-limits
+* https://developer.github.com/v3/#rate-limiting
+* https://developer.github.com/v3/rate_limit/
+* https://developer.github.com/v4/guides/resource-limitations/
+* https://developer.github.com/v3/#increasing-the-unauthenticated-rate-limit-for-oauth-applications
+
+
+---
+
+#### Credits
+
+Most grateful thanks to:
+* [---](https://github.com/---/) - for clarifying and fixing XXXX
+
+---
+
+#### License
+
+[![GitHub license](https://img.shields.io/github/license/E3V3A/gh-missue.svg)](https://github.com/E3V3A/gh-missue/blob/master/LICENSE)
+
+A license to :sparkling_heart:!
+
---
+++ gh-missue.rb
@@ -0,0 +1,315 @@
+#!/usr/bin/env ruby
+# gh-missue.rb -- A GitHub issue migration script written in Ruby
+#==================================================================================================
+# Author:       E:V:A
+# Date:         2018-04-10
+# License:      ISC
+# Formatting    UTF-8 with 4-space TAB stops and no TAB chars.
+# URL:          https://github.com/E3V3A/gh-missue
+# Based On:     https://github.com/TimothyBritt/github-issue-migrate
+# 
+# Description:  A Ruby script for migrating selected GitHub issues to your own repository and 
+#               using OAuth2 authentication to increase speed and prevent rate limiting.
+#
+# Dependencies:
+#       [1] docopt  https://github.com/docopt/docopt.rb/
+#       [2] octokit https://github.com/octokit/octokit.rb/
+# 
+# NOTE:
+#
+#   1. To make this run, you need to:
+#           (a) have Ruby installed
+#           (b) gem install GitHubs own "octokit" library
+#           (c) gem install the option parser "docopt" 
+#   2. You should also consider creating a personal authentication token on GitHub,
+#      to avoid getting rate-limited by a large number of requests in short time.
+#
+# ToDo: 
+#   
+#   [ ] Fix username/password authentication
+#   [ ] Fix inclusion of CLI options: -d, -n  
+#       -d              - show debug info with full option list, raw requests & responses etc.
+#       -n  <1,3-6,9>   - only migrate issues given by the list. Can be a range.
+#       
+#==================================================================================================
+
+# Use Ruby3 "frozen" in Ruby2
+# frozen_string_literal: true
+
+#require './lib/issue_migrator.rb'
+#require 'pp
+
+require 'docopt'
+require 'octokit'
+
+require 'net/http'
+require 'uri'
+require 'json'
+
+VERSION = '1.0.1'
+options = {}
+
+#--------------------------------------------------------------------------------------------------
+# The cli options parser
+#--------------------------------------------------------------------------------------------------
+doc = <<DOCOPT
+
+  Description:
+
+    gh-missue is a Ruby program that migrate issues from one github repository to another.
+    Please note that you can only migrate issues to your own repo, unless you have an OAuth2
+    authentication token.
+
+  Usage:
+        #{__FILE__} [-c] [-n <ilist> | -t <itype>] <source_repo> <target_repo>
+        #{__FILE__} [-c] [-n <ilist> | -t <itype>] <oauth2_token> <source_repo> <target_repo>
+        #{__FILE__} [-c] [-n <ilist> | -t <itype>] <username> <password> <source_repo> <target_repo>
+        #{__FILE__} [-d] -l <itype> [<oauth2_token>] <repo>
+        #{__FILE__} -n <ilist>
+        #{__FILE__} -t <itype>
+        #{__FILE__} -r [<oauth2_token>]
+        #{__FILE__} -d
+        #{__FILE__} -v
+        #{__FILE__} -h
+
+  Options:
+
+        -c                  - only copy all issue labels from <source> to <target> repos, including name, color and description
+        -l <itype> <repo>   - list available issues of type <itype> (all,open,closed) and all labels in repository <repo>
+        -t <itype>          - specify what type (all,open,closed) of issues to migrate. [default: open]
+        -r                  - show current rate limit and authentication method for your IP
+        -d                  - show debug info with full option list, raw requests & responses etc.
+        -n <ilist>          - only migrate issues with comma separated numbers given by the list. Can include a range.
+        -h, --help          - show this help message and exit
+        -v, --version       - show version and exit
+
+  Examples:
+
+        #{__FILE__} -r
+        #{__FILE__} -l open E3V3A/MMM-VOX
+        #{__FILE__} -t closed "E3V3A/TESTO" "USERNAME/REPO"
+        #{__FILE__} -n 1,4-5 "E3V3A/TESTO" "USERNAME/REPO"
+
+  Dependencies:
+        #{__FILE__} depends on the following gem packages: octokit, docopt.
+        It also depend on json, net/http, uri, but these are usually already installed.
+
+DOCOPT
+
+#--------------------------------------------------------------------------------------------------
+# The IssueMigrator
+#--------------------------------------------------------------------------------------------------
+class IssueMigrator
+
+    # attr_accessor :issues, :client, :target_repo, :source_repo
+    attr_accessor :access_token, :issues, :ilist, :itype, :client, :target_repo, :source_repo
+
+    def initialize(access_token, source_repo, target_repo)
+        @client = Octokit::Client.new( :access_token => access_token, 
+            # :accept => 'application/vnd.github.symmetra-preview+json',
+            # :headers => { "X-GitHub-OTP" => "<your 2FA token>" }
+            per_page: 100 )
+
+            # // Personal OAuth2 Access Token
+            #:access_token => "YOUR_40_CHAR_OATH2_TOKEN"
+
+            # // Standard GitHub Credentials
+            #:login => username,
+            #:password => password
+
+            # // OAuth2 App Credentials
+            #:client_id     => "<YOUR_20_CHAR_OATH2_ID>",
+            #:client_secret => "<YOUR_40_CHAR_OATH2_SECRET>"
+
+        user = client.user
+        user.login
+        @source_repo = source_repo
+        @target_repo = target_repo
+        @itype = itype
+    end
+
+    def pull_source_issues(itype) # ilist => nil ??
+        @client.auto_paginate = true
+        @issues = @client.issues(@source_repo, :state => itype)     # The issue type:   <itype>: [open/closed/all]
+        # @issues = @client.issues(@source_repo, :issue => ilist)   # The issue list:   <ilist>: "1,2,5-7,19"
+        puts "Found #{issues.size} issues of type: #{itype}\n"
+    end
+
+    def list_source_issues(itype)
+        pull_source_issues(itype)
+        @issues.each do |source_issue|
+            puts "[#{source_issue.number}]\t  #{source_issue.title}"
+        end
+        puts
+    end
+
+    def list_source_labels
+        @client.auto_paginate = true
+        @labels = @client.labels(@source_repo.freeze, accept: 'application/vnd.github.symmetra-preview+json')
+        puts "Found #{@labels.size} issue labels:"
+        @labels.each do |label|
+            puts "[#{label.color}]  #{label.name} :  #{label.description}"
+        end
+        puts
+    end
+
+    def create_target_labels
+        @client.auto_paginate = true
+        @source_labels = @client.labels(@source_repo.freeze, accept: 'application/vnd.github.symmetra-preview+json')
+        # @target_labels = @client.add_label(@target_repo.freeze, accept: 'application/vnd.github.symmetra-preview+json')
+        puts "Found #{@source_labels.size} issue labels in <source_repo>:"
+        puts "Copying labels..."
+        tlabel = "" # nil
+        @source_labels.each do |lbl|
+            puts "[#{lbl.color}]  #{lbl.name} :  #{lbl.description}"
+            #tlabel = {"name": lbl.name, "description": lbl.description, "color": lbl.color}
+            #tlabel = {lbl.name, lbl.color, description: lbl.description}
+            #lab = client.add_label(@target_repo.freeze, accept: 'application/vnd.github.symmetra-preview+json', tlabel)
+            lab = client.add_label(@target_repo.freeze, lbl.name, lbl.color, accept: 'application/vnd.github.symmetra-preview+json', description: lbl.description)
+            sleep(2)
+        end
+        puts "done."
+    end
+
+    def push_issues
+        @issues.reverse!
+        n = 0
+        @issues.each do |source_issue|
+            n += 1
+            print "Processing issue: #{source_issue.number}  (#{n}/#{issues.size})\r"
+            source_labels = get_source_labels(source_issue)
+            source_comments = get_source_comments(source_issue)
+            if !source_issue.key?(:pull_request) || source_issue.pull_request.empty?
+                target_issue = @client.create_issue(@target_repo, source_issue.title, source_issue.body, {labels: source_labels})
+                push_comments(target_issue, source_comments) unless source_comments.empty?
+                @client.close_issue(@target_repo, target_issue.number) if source_issue.state === 'closed'
+            end
+            # We need to set a rate limit, even for OA2, it is 0.5 [req/sec]
+            sleep(90) if ( issues.size > 1 ) # [sec]
+        end
+        puts "\n"
+    end
+
+    # API bug:  missing color/description
+    def get_source_labels(source_issue)
+        labels = []
+        source_issue.labels.each do |lbl|
+            labels << {"name": lbl.name, "description": lbl.description, "color": lbl.color}
+        end
+        #puts "Labels: #{labels}"
+        labels
+    end
+
+    def get_source_comments(source_issue)
+        comments = []
+        source_comments = @client.issue_comments(@source_repo, source_issue.number)
+        source_comments.each do |cmt|
+            comments << cmt.body
+        end
+        comments
+    end
+
+    def push_comments(target_issue, source_comments)
+        source_comments.each do |cmt|
+            @client.add_comment(@target_repo, target_issue.number, cmt)
+        end
+    end
+end
+
+#--------------------------------------------------------------------------------------------------
+# MAIN
+#--------------------------------------------------------------------------------------------------
+
+#if __FILE__ == $0
+begin
+
+    def sort_list(ilist)
+        # "12,3-5,2,6,35-38" --> [2,3,4,5,6,12,35,36,37,38]
+        ilist.gsub(/(\d+)-(\d+)/) { ($1..$2).to_a.join(',') }.split(',').map(&:to_i).sort.uniq
+    end
+
+    options = Docopt::docopt(doc, version: VERSION) # help: true
+
+    if options['-d']
+        #pp Docopt::docopt(doc, version: VERSION)
+        puts "\nAvailable options are:\n#{options.inspect}\n"
+        puts "\nThe supplied CLI options were:\n#{ARGV.inspect}\n\n"
+    end
+
+    if options['<oauth2_token>'] 
+        access_token = options['<oauth2_token>']
+        if access_token.size != 40 
+            puts "Error: The github access token has to be 40 characters long!"
+            exit
+        else 
+            puts "Using access_token: #{access_token}" if options['-d']
+        end
+    end
+
+    # -l <itype> <source_repo>
+    if ( options['-l'] )
+        itype = options['-l']
+        source_repo =  options['<repo>']
+        target_repo = "E3V3A/TESTT" # a dummy repo
+        im = IssueMigrator.new("#{access_token}", "#{source_repo}", "#{target_repo}")
+        im.list_source_issues(itype)
+        im.list_source_labels
+    end
+
+    # -n <ilist>
+    if ( options['-n'] )
+        ilist = options['-n']
+        puts "The \"-n\" option has not yet been implemented!"
+        puts "The supplied issue list: #{ilist}"
+        #sorted = ilist.split(",").sort_by(&:to_i)
+        sorted = sort_list(ilist)
+        puts "The sorted issue list  : #{sorted}"
+    end
+
+    # -r / curl -i -G 'https://api.github.com/rate_limit?access_token=xxxx'
+    if ( options['-r'] )
+        if (access_token)
+            puts "Using access_token: #{access_token}" 
+            uri = URI.parse("https://api.github.com/rate_limit?access_token=#{access_token}")
+        else 
+            uri = URI.parse("https://api.github.com/rate_limit")
+        end
+        res = Net::HTTP.get_response(uri) 
+        if (res.code != "200")
+            puts "ERROR: Bad reponse code: #{res.code}\n"
+            puts res.body
+        else
+            debug = false
+            if (debug)
+                puts "\nResponse:\nHeader:\n#{res.header}\n}" # not working?
+                puts "Body:\n#{res.body}\n\n}"
+            end
+            rbr = JSON.parse(res.body)['rate']
+            RT = Time.at(rbr['reset'])
+            puts "Rate limit : #{rbr['limit']}"
+            puts "Remaining  : #{rbr['remaining']}"
+            puts "Refresh at : #{RT}"
+        end
+    end
+    
+    # MAIN
+    if ( options['<source_repo>'] and options['<target_repo>'] )
+        itype = options['-t']
+        #ilist = options['-n']
+        #puts "<itype>: #{itype}"  # debug
+        source_repo = options['<source_repo>']
+        target_repo = options['<target_repo>']
+        im = IssueMigrator.new("#{access_token}", "#{source_repo}", "#{target_repo}")
+        im.create_target_labels
+        exit if options['-c']
+        im.pull_source_issues(itype)    # add ilist
+        #im.list_source_issues(itype)   # 
+        im.push_issues
+    end
+
+rescue Docopt::Exit => e
+    puts e.message
+    #puts e.backtrace.inspect
+end
+
+puts "\nDone!\n"
